5ee5d1c3f9ea3f54568b3a0053c49b7dfae0209e
eyos <eyosiastamirat20@gmail.com>
2023-01-02 03:34:28 +0300

    screenshot added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 async function run(){
- const browser = await puppeteer.launch({headless: false});
+const browser = await puppeteer.launch({headless: false});
 const page = await browser.newPage();
 await page.goto('https://checkboxrace.com/');
 await delay(1000);
